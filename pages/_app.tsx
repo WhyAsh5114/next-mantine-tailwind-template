72e1e6001a0051996966a18e7a38e4edce40359b
WhyAsh5114 <whyash5114@gmail.com>
2022-07-13 01:07:56 +0530

    mantine and tailwind setup in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Project Title</title>
+        <meta name="description" content="Project description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp
